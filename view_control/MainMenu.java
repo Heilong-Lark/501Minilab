bf7733fc3e79ab2af8c92d2e05ef16910e4fee70
grace <grace@Grace-HP>
2020-04-29 20:34:56 -0700

    Deleted classes

--- view_control/MainMenu.java
+++ view_control/MainMenu.java
@@ -64,69 +64,6 @@ public class MainMenu extends JFrame {
 		JMenu mnHone = new JMenu("Home");
 		menuBar.add(mnHone);
 		
-		JMenuItem mntmPreferences = new JMenuItem("Preferences");
-		mnHone.add(mntmPreferences);
-		
-		JMenu mnApTest = new JMenu("AP Test");
-		menuBar.add(mnApTest);
-		
-		JMenuItem mntmApReview = new JMenuItem("Review");
-		mntmApReview.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				AP_UI frame = new AP_UI();
-				frame.setVisible(true);
-			}
-		});
-		mnApTest.add(mntmApReview);
-		
-		JMenu mnJig = new JMenu("Labs");
-		menuBar.add(mnJig);
-		
-		JMenuItem mntmCalculator = new JMenuItem("Calculator");
-		mntmCalculator.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				CalculatorUI frame = new CalculatorUI();
-				frame.setVisible(true);		
-			}
-		});
-		mnJig.add(mntmCalculator);
-		
-		JMenuItem mntmMethodSearch = new JMenuItem("File Search");
-		mntmMethodSearch.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				MethodSearchUI frame = new MethodSearchUI();
-				frame.setVisible(true);
-			}
-		});
-		
-		JMenuItem mntmPalindrome = new JMenuItem("Palindrome");
-		mntmPalindrome.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				PalindromeUI frame = new PalindromeUI();
-				frame.setVisible(true);
-			}
-		});
-		mnJig.add(mntmPalindrome);
-		mnJig.add(mntmMethodSearch);
-		
-		JMenuItem mntmProgrammingLanguages = new JMenuItem("Model Search");
-		mntmProgrammingLanguages.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				ProgLang_UI frame = new ProgLang_UI();
-				frame.setVisible(true);
-			}
-		});
-		
-		JMenuItem mntmNewMenuItem = new JMenuItem("Method Show");
-		mntmNewMenuItem.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				MethodShowUI frame = new MethodShowUI();
-				frame.setVisible(true);
-			}
-		});
-		mnJig.add(mntmNewMenuItem);
-		mnJig.add(mntmProgrammingLanguages);
-		
 		
 	}
 	
